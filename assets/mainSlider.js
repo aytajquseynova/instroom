24b7317ec3dfcf060d5b67309169219a15717126
aytajquseynova <you@example.com>
2023-09-27 14:46:27 +0400

    js slider

---
+++ assets/mainSlider.js
@@ -0,0 +1,16 @@
+let slider_img = document.querySelector('.slider-img');
+let images = ['1.png', '2.png'];
+let i = 0;
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    i++;
+    if (i >= images.length) {
+        i = 0;
+    }
+    setImg();
+});
+
+function setImg() {
+    return slider_img.setAttribute('src', "./assets/images/" + images[i]);
+
+}
